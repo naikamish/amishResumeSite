38f947476d6f68be479e9a3d7221a63ac256d808
naikamish <naikamish0822@gmail.com>
2015-10-15 23:00:36 -0400

    Add resume page to website

---
+++ amishResumeSite/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <img src="images/banner_tree_tale_color_crea-f.png" style="width:500px; height:auto;">
+        <br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <!About Me Section-->
+        <br /><br />
+        <div style="background-color:#ffeb3b; display:inline-block; width:100%; text-align: center; color:#000000;">
+            <br /><br />
+            <div style="display:inline-block; width:200px; vertical-align: top;">ABOUT</div>
+            <div style="display:inline-block; width:800px; vertical-align: top;">I am an aspiring software and web developer who has experience working in Java and PHP on the back-end and HTML, CSS, and Javascript on the front-end. I am also working towards a Master of Computer Information Systems degree from Sacred Heart University. My interests are programming, movies, and TV.</div>
+            <br /><br /><br />
+        </div>
+        <br /><br /><br />
+        <!--Education Section-->
+        <div style="display:inline-block; width:200px; vertical-align: top;">EDUCATION</div>
+        <div style="display:inline-block; width:800px; vertical-align: top;">
+            <div style="display:inline-block; width:600px;">
+                <h2>Sacred Heart University</h2>
+                <h3>Master of Computer Information Systems</h3>
+            </div>
+            <div style="display:inline-block; width:195px;">
+                <h3>May 2016</h3>
+            </div>
+            <br /><br />
+            <div style="display:inline-block; width:600px;">
+                <h2>University of Connecticut</h2>
+                <h3>Bachelor of Science in Actuarial Science</h3>
+            </div>
+            <div style="display:inline-block; width:195px;">
+                <h3>May 2013</h3>
+            </div>
+        </div>
+        <br /><br /><br /><br /><br /><br />
+        <!--Experience Section-->
+        <div style="display:inline-block; width:200px; vertical-align: top;">EXPERIENCE</div>
+        <div style="display:inline-block; width:800px; vertical-align: top;">
+            <div style="display:inline-block; width:600px;">
+                <h2>Netc LLC.</h2>
+                <h3>Web Design Intern</h3>
+                <ul>
+                    <li>Independently redesigned the company's website in order to make it more user friendly for customers</li>
+                    <li>Work on front end design using HTML, CSS, Javascript, and JQuery</li>
+                    <li>Also worked on back end aspects with PHP</li>
+                </ul>
+            </div>
+            <div style="display:inline-block; width:195px;">
+                <h3>March 2015 - Present</h3>
+            </div>
+            <br /><br />
+            <div style="display:inline-block; width:600px;">
+                <h2>A100</h2>
+                <h3>Software Developer Apprentice</h3>
+                <ul>
+                    <li>Created web applications using NodeJS, Meteor, and MongoDB</li>
+                    <li>Worked on team projects using agile methodology for software development</li>
+                    <li>Learned to use git as a version control system</li>
+                </ul>
+            </div>
+            <div style="display:inline-block; width:195px;">
+                <h3>October 2015 - Present</h3>
+            </div>
+        </div>
+        <br /><br />
+        <!--Skills Section-->
+        <div style="background-color:#ffeb3b; display:inline-block; width:100%; text-align: center; color:#000000;">
+            <br /><br />
+            <div style="display:inline-block; width:200px; vertical-align: top;">SKILLS</div>
+            <div class="skills-section" style="display:inline-block; width:800px; vertical-align: top;">
+                <div style="display:inline-block; width:195px;">
+                    <img src="images/Java_logo.png" style="display:inline-block; height:100px; width:auto;">
+                    <br />
+                    <p>Java</p>
+                </div>
+                <div style="display:inline-block; width:195px;">
+                    <img src="images/cplusplus_logo.png" style="display:inline-block; height:100px; width:auto;">
+                    <br />
+                    <p>C++</p>
+                </div>
+                <div style="display:inline-block; width:195px;">
+                    <img src="images/html-css-javascript_logo.png" style="display:inline-block; height:100px; width:auto;">
+                    <br />
+                    <p>HTML/CSS/Javascript</p>
+                </div>
+                <div style="display:inline-block; width:195px;">
+                    <img src="images/php-mysql_logo.png" style="display:inline-block; height:100px; width:auto;">
+                    <br />
+                    <p>PHP/MySql</p>
+                </div>
+            </div>
+            <br /><br />
+        </div>
+        <br /><br />
+        <!--Contact Section-->
+        <br /><br />
+        <div style="display:inline-block; width:200px; vertical-align: top;">CONTACT</div>
+        <div style="display:inline-block; width:265px; vertical-align: top; text-align:center;">
+            <span style="font-weight:bold;">Email</span><br>
+            <span>naikamish0822@gmail.com</span>
+        </div>
+        <div style="display:inline-block; width:265px; vertical-align: top; text-align:center;">
+            <span style="font-weight:bold;">Phone</span><br>
+            <span>203-913-5804</span>
+        </div>
+        <div style="display:inline-block; width:265px; vertical-align: top; text-align:center;">
+            <span style="font-weight:bold; text-align:left">Git</span><br>
+            <span>https://github.com/naikamish</span>
+        </div>
+        <br /><br /><br />
+    </body>
+</html>
